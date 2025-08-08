5c8a8be832add2229df36f67a0976c6b34ce6191
AleemaSaleem <116712791+AleemaSaleem@users.noreply.github.com>
2025-08-08 10:01:52 +0500

    Delete panaversity/prompt.py

--- panaversity/prompt.py
+++
@@ -1,9 +0,0 @@
-from agents import RunContextWrapper, Agent
-from context import UserContext
-
-def dynamic_prompt(ctx: RunContextWrapper[UserContext], agent: Agent) -> str:
-    return (
-        f"You are a research assistant called {agent.name}. "
-        f"The user is {ctx.context.username}. "
-        "You have access to web search. Use it to answer complex research queries."
-    )
