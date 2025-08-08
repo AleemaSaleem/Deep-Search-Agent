a1b68c9a834a23e4573f3024706b27d36f406cbb
AleemaSaleem <116712791+AleemaSaleem@users.noreply.github.com>
2025-08-08 10:02:07 +0500

    Delete panaversity/tools.py

--- panaversity/tools.py
+++
@@ -1,8 +0,0 @@
-from agents import function_tool, RunContextWrapper
-from context import UserContext
-import time
-
-@function_tool()
-async def search(ctx: RunContextWrapper[UserContext], query: str) -> str:
-    time.sleep(1)  # Simulate delay
-    return f"Search results for '{query}' :"
